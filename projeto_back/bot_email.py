44210b242ee30fb27b9f6e57154fb2aea9d731b3
Guilherme Kalleu Novais <guilhermekalleuofc@gmail.com>
2024-11-01 18:53:42 -0300

    Resolvido comando para enviar email

--- projeto_back/bot_email.py
+++ projeto_back/bot_email.py
@@ -1,19 +1,46 @@
-import win32com.client as win32
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+import email.message
 
-try:
-    outlook = win32.Dispatch('outlook.application')
-    email = outlook.CreateItem(0)
-    email.To = "guilhermekalleu@gmail.com"
-    email.Subject = "Cadastro no Reciclapp"
-    email.HTMLBody = """
+def enviar_token(destinatario, assunto, mensagem):
+
+    smtp_server =  "smtp.gmail.com"
+    smtp_port = 587
+    remetente = "reciclappinc@gmail.com"
+    senha = "eapt dtpl vjjy ctce"
+
+    msg =  MIMEMultipart()
+    msg['From'] = remetente
+    msg['To'] = destinatario
+    msg['Subject'] = assunto
+
+    msg.attach(MIMEText(mensagem, 'html'))
+
+    try:
+        server = smtplib.SMTP(smtp_server, smtp_port)
+        server.starttls()
+        server.login(remetente, senha)
+        server.sendmail(remetente, destinatario, msg.as_string())
+        print("Email enviado com sucesso!")
+
+    except Exception as e:
+        print(f"Erro ao enviar email: {e}")
+
+    finally:
+        server.quit()
+
+destinatario = "ana.pastor789@gmail.com"
+corpo_email = """
     <p>Olá "Usuario",</p>
+    <p></p>
     <p>Você fez um cadastro no Reciclapp. Para confirmar seu cadastro você deve validar sua conta com este token.</p>
     <p>Este procedimento é necessário para sua segurança e o melhor funcionamento do aplicativo.</p>
     <p>Token para validação: token</p>
     <p>Agora como membro registrado você tem um login exclusivo e acesso total as funcionalidades do app, pode usar!</p>
-    <p>* Mensagem automática, favor não responder.</p>
+    <p></p>
+    * Mensagem automática, favor não responder.</p>
     """
-    email.Send()
-    print("Email enviado com sucesso!")
-except Exception as e:
-    print(f"Ocorreu um erro: {e}")
+
+    
+enviar_token(destinatario, "Confirmar registro" , corpo_email)
